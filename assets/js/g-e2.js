6777793cdce7932537de7e5b304897eca2982f7f
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-12 10:42:28 +0100

    exo 2.4, fonctionne, j'ai fait les choses à ma sauce

--- assets/js/g-e2.js
+++ assets/js/g-e2.js
@@ -1,8 +1,20 @@
 const input = document.querySelector("input");
 input.addEventListener("keyup", fonctionResponse);
+input.addEventListener("keyup", fonctionResponse2);
 const div = document.createElement("div");
 document.body.insertBefore(div, document.querySelector("script"));
 
 function fonctionResponse(){
     div.innerHTML = "Voici ce que vous avez saisi : <br>" + this.value;
+}
+
+function fonctionResponse2(){
+    if(this.value.includes("Bonjour")){
+        input.classList.remove("RED");
+        input.classList.add("GREEN");
+    }
+    else{
+        input.classList.remove("GREEN");
+        input.classList.add("RED");
+    }
 }
